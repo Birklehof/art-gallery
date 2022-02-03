d6a8e460695dfe8f99376915c18763fd009956ca
Paul Maier <pauljustus279@gmail.com>
2022-02-03 21:24:46 +0000

    initial gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -1,13 +1,13 @@
-var ghpages = require('gh-pages');
+let ghpages = require('gh-pages');
 
 ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository
+        repo: 'https://github.com/Birklehof/Art-Gallery.git', // Update to point to your repository
         user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
+            name: 'Paul Maier', // update to use your name
+            email: 'pauljustus279@gmail.com' // Update to use your email
         }
     },
     () => {
