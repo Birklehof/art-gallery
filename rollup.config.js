1234adaac1d726df33b9f45629abc3fcb8b9f56a
Paul Maier <pauljustus279@gmail.com>
2022-02-11 20:10:15 +0000

    Better styling

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
 // const production = !process.env.ROLLUP_WATCH;
-const production = false;
+const production = true;
 
 function serve() {
 	let server;
